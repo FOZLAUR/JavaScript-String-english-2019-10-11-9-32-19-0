ec2e4d5e69c5fc672d9b044e2d58dfe031150b6c
FOZLAUR <fozla@oocl.com>
2020-10-02 01:43:24 +0800

    updated index.js with final answer

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
-// TODO write your code here...
-
+var result = name.toUpperCase();
+window.alert(result);
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
-//TODO write your code here...
-
+var sTrim = sentence.trim();
+var result = "";
+result += sTrim.charAt(0).toUpperCase();
+for(var i=1; i<sTrim.length; i++){
+	var prev = sTrim.charAt(i-1);
+	var curr = sTrim.charAt(i);
+	if (prev==" "){
+		curr=curr.toUpperCase();
+	}
+    result+=curr;
+}
+window.alert(result);
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
-//TODO write your code here...
+var result = "";
+for (var i=0; i<money.length; i++){
+	var temp = money.charAt(i);
+    if(temp.match(/\d/)){
+        result += temp
+    }
+}
+window.alert(result);
